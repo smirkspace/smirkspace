ad98c6588110b54b56949fd36553064438f57638
TimBaker <tbakerx@gmail.com>
2016-10-27 18:06:08 -0700

    Got some things working for the text chat but not configured properly and styling is off

--- client/main.js
+++ client/main.js
@@ -33,10 +33,50 @@ Meteor.startup(() => {
         <IndexRedirect to="/dashboard" />
           <Route path="travel" component={SpaceFrame}/>
         </Route>
-        
+
         <Route path='*' component={Splash} />
       </Route>
     </Router>,
     document.getElementById('render-target')
   );
 });
+
+
+
+
+SimpleChat.configure ({
+    texts:{
+        loadMore: 'Load More',
+        placeholder: 'Type message ...',
+        button: 'send',
+        join: 'Join to',
+        left: 'Left the',
+        room: 'room at'
+
+    },
+    limit: 5,
+    beep: true,
+    showViewed: true,
+    showReceived: true,
+    showJoined: true,
+    publishChats: function(roomId, limi){ //server
+       //here the context is the same for a Publications, that mean you have access to this.userId who are asking for subscribe.
+       // for example
+       return isLoggedAndHasAccessToSeeMessage(this.userId)
+    },
+    allow: function(message, roomId, username, avatar, name){
+       //here the context is the same for a Methods, thats mean you hace access to this.userId also
+       // for example
+       return isLoggedAndHasAccessSendMessages(this.userId)
+        return true
+    },
+    onNewMessage:function(msg){  //both
+    },
+    onReceiveMessage:function(id, message, room){ //server
+
+    },
+    onJoin:function(roomId, username, name,date){  //server
+    },
+    onLeft:function(roomId, username, name,date) { //server
+    }
+})
--- imports/ui/SpaceChat/index.js
+++ imports/ui/SpaceChat/index.js
@@ -1,23 +1,83 @@
 import React, { Component } from 'react';
+import { Template } from 'meteor/templating';
+import Blaze from 'meteor/gadicc:blaze-react-component';
+
+
+
+SimpleChat.configure({
+    texts:{
+        loadMore: 'Load More',
+        placeholder: 'Type message ...',
+        button: 'send',
+        join: 'Join to',
+        left: 'Left the',
+        room: 'room at'
+
+    },
+    limit: 5,
+    beep: true,
+    showViewed: true,
+    showReceived: true,
+    showJoined: true,
+    
+    allow: function(message, roomId, username, avatar, name){
+       //here the context is the same for a Methods, thats mean you hace access to this.userId also
+       // for example
+       return isLoggedAndHasAccessSendMessages(this.userId)
+        return true
+    },
+    onNewMessage:function(msg){  //both
+    }
+
+})
+
 
 export default class SpaceChat extends Component {
-  render() {
+
+
+
+render() {
     return(
-      <div>
-        <header className="intro">
-          <div className="intro-body">
-            <div className="container">
-              <div className="row">
-                <div className="col-md-8 col-md-offset-2">
-                  <h1 className="brand-heading">Space</h1>
-                  <p className="intro-text">Helping you connect with students near you!<br />Find your space.</p>
-                  <br /><br /><br />
-                </div>
+      <div class="container">
+
+          <div class="row" >
+              <div class=" col-md-8 col-md-offset-2" >
+
+                      <div class="box box-info direct-chat direct-chat-info">
+                          <div class="box-header with-border">
+                              <h3 class="box-title">Room: test</h3>
+                              <div class="box-tools pull-right">
+                                <button class="btn btn-box-tool" data-widget="collapse">Somebody New<i class="fa fa-minus"></i>
+                                </button>
+                              </div>
+                          </div>
+                          <Blaze template="SimpleChatWindow" roomId={"test"} username={"testUser"}/>
+                      </div>
+
               </div>
-            </div>
           </div>
-        </header>
-      </div>
+
+
+          </div>
     );
   }
 }
+
+
+
+
+/*<div>
+  <header className="intro">
+    <div className="intro-body">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8 col-md-offset-2">
+            <h1 className="brand-heading">Space</h1>
+            <p className="intro-text">Helping you connect with students near you!<br />Find your space.</p>
+            <br /><br /><br />
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+</div>*/

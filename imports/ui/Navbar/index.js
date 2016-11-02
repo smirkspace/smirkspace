0d14d69592b764c0fd573485212143c43306dd32
HaltonO <om.halton@gmail.com>
2016-11-01 20:30:51 -0700

    Adds logo to NavBar

--- imports/ui/Navbar/index.js
+++ imports/ui/Navbar/index.js
@@ -2,29 +2,27 @@ import React, { Component } from 'react';
 import { Link } from 'react-router'
 import { Meteor } from 'meteor/meteor';
 import AccountsUIWrapper from '../AccountsUIWrapper';
+import './index.css';
 
 export default class Navbar extends Component {
-	render() {
-    	return(
-	        <nav className="navbar navbar-inverted">
-	          <div className="container">
-	            <div className="navbar-header">
-	                <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-	                    <span className="sr-only">Toggle navigation</span>
-	                    <span className="icon-bar"></span>
-	                    <span className="icon-bar"></span>
-	                    <span className="icon-bar"></span>
-	                </button>
-	                <h1><Link to="/dashboard" className="navbar-brand">SmirkSpace</Link></h1>
-	            </div>
-	            <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1" className="lead">
-	                <ul className="nav navbar-nav navbar-right">
-	                <li><button type="button" className="btn navbar-btn"><AccountsUIWrapper/></button></li>
-	                </ul>
-	            </div>
-	          </div>
-	        </nav>
-
-	    );
-	}
+  render () {
+    return (
+      <nav className="navbar navbar-inverted">
+       <div className="container">
+        <div className="navbar-header">
+         <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+          <span className="sr-only">Toggle navigation</span>
+          <span className="icon-bar"></span>
+          <span className="icon-bar"></span>
+          <span className="icon-bar"></span>
+         </button>
+         <Link to="/dashboard"><img src="images/logo2.png" width="140" height="100"></img><h3 className="brandName">Smirkspace</h3></Link>
+        </div>
+        <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1" className="lead">
+         <ul className="nav navbar-nav navbar-right">
+          <li><button type="button" className="btn navbar-btn"> <AccountsUIWrapper /> </button> </li>
+         </ul>
+        </div>
+       </div>
+     </nav>) }
 }

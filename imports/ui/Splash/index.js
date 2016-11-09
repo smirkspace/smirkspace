25cd3796926c34343ae3ed9b6c3950f8691f129b
HaltonO <om.halton@gmail.com>
2016-11-08 18:31:02 -0800

    image fixing

--- imports/ui/Splash/index.js
+++ imports/ui/Splash/index.js
@@ -11,6 +11,27 @@ export default class Splash extends Component {
                 <div className="col-md-8 col-md-offset-2">
                   <h1 className="brand-heading">SmirkSpace</h1>
                   <p className="intro-text">Connecting People Through Conversation</p>
+                                    <p className="intro-text">Enter a space below to start chatting!</p>
+                  <p className="intro-text">Enter a space below to start chatting!</p>
+                  <p className="intro-text">Enter a space below to start chatting!</p>
+                  <p className="intro-text">Enter a space below to start chatting!</p>
+                  <p className="intro-text">Enter a space below to start chatting!</p>
+                  <p className="intro-text">Enter a space below to start chatting!</p>
+                  <p className="intro-text">Enter a space below to start chatting!</p>
+                  <p className="intro-text">Enter a space below to start chatting!</p>
+                  <p className="intro-text">Enter a space below to start chatting!</p>
+                  <p className="intro-text">Enter a space below to start chatting!</p>
+                  <p className="intro-text">Enter a space below to start chatting!</p>
+                  <p className="intro-text">Enter a space below to start chatting!</p>
+                  <p className="intro-text">Enter a space below to start chatting!</p>
+                  <p className="intro-text">Enter a space below to start chatting!</p>
+                  <p className="intro-text">Enter a space below to start chatting!</p>
+                  <p className="intro-text">Enter a space below to start chatting!</p>
+                  <p className="intro-text">Enter a space below to start chatting!</p>
+                  <p className="intro-text">Enter a space below to start chatting!</p>
+                  <p className="intro-text">Enter a space below to start chatting!</p>
+
+
                 </div>
               </div>
             </div>

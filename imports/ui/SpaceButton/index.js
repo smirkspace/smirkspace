d8414f4c52a67977d2d6112f208c13a1e8bcb8be
Madeline Petersen <madeline.petersen@metalabdesign.com>
2016-11-15 18:57:58 -0800

    Adds props to Button function declaration

--- imports/ui/SpaceButton/index.js
+++ imports/ui/SpaceButton/index.js
@@ -1,17 +1,16 @@
-// imports/ui/Button/index.js
 import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
 import './index.css';
 
-export default function Button() {
+export default function Button(props) {
   return (
-    <Link to={this.props.link}>
-      <input type="image" className="spaceButton" src={this.props.source} />
+    <Link to={props.link}>
+      <input type="image" className="spaceButton" src={props.source} />
     </Link>
   );
 }
 
 Button.propTypes = {
-  source: PropTypes.string.isRequired,
   link: PropTypes.string.isRequired,
+  source: PropTypes.string.isRequired,
 };

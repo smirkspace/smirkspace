e83fc71dd1daba432334b0c29a2b2c97dbc85b63
Dahv Reinhart <dahvreinhart@hotmail.com>
2016-11-18 15:10:37 -0800

    implemented fake door counters for space buttons and report/video buttons

--- imports/ui/SpaceButton/index.js
+++ imports/ui/SpaceButton/index.js
@@ -1,26 +1,35 @@
 import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
 import './index.css';
-
-// Function to give alerts for buttons not yet made
-// And to incrememnt counters for fake doors
-function spaceNotAvailable() {
-  alert('This space is still being developed.\nCheck back soon!');
-  // increment counters collection here....
-}
+import { countHandler, Counters } from '../../api/CountersDbCollection'
 
 export default function Button(props) {
+
+  // Function to give alerts for buttons not yet made
+  // And to incrememnt counters for fake doors
+  function spaceNotAvailable() {
+    
+    //call function within the CountersDbCollection file
+    //will create a DB object if button is being clicked for the first time
+    //or will update button count if the object already exists
+    countHandler(props.buttonName);
+
+    //print out a message to the user
+    alert('This space is still being developed.\nCheck back soon!');
+  }
+
   const buttonType = props.link
   ? (
     <Link to={props.link}>
       <input type="image" className="spaceButton" src={props.source} />
     </Link>)
-  : <input type="image" className="spaceButton" src={props.source} onClick={spaceNotAvailable} />;
+  : <input type="image" className="spaceButton" src={props.source} onClick={spaceNotAvailable} />
 
   return buttonType;
 }
 
 Button.propTypes = {
   link: PropTypes.string,
+  buttonName: PropTypes.string.isRequired,
   source: PropTypes.string.isRequired,
 };

96abd6455fcbdb664dae388f1551a17aeca1ea25
bridgetrass <bridget.rassell@gmail.com>
2016-11-24 12:39:43 -0800

    added scroll to sections on splash page

--- imports/ui/Guide/index.js
+++ imports/ui/Guide/index.js
@@ -8,21 +8,21 @@ export default function Guide() {
       <p>&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;</p>
       <p>&nbsp; &nbsp;&nbsp; &nbsp;&nbsp;</p>
 
-      <div className="div1">
+      <div className="whatwedo" id="whatwedo">
       <img className="img" width="400" height="300" src="/images/exa.png" />
         <h1 className="one">Turn any email provider into a growth marketing platform.</h1>
 
         <p className="paragraph1">A beautiful dashboard to manage all your templates and a simple REST API to automate everything.
         </p>
       </div>
-    <div className="div2">
+    <div className="usecase" id="usecase">
     <img className="img" width="400" height="300" src="/images/exa.png" />
 
       <h1 className="two">Email is a powerful part of your customer experience..</h1>
       <p className="paragraph2">Your product speaks to customers through email. Notifications, Invitations, and Payment Receipts
       are critical to your user experience.</p>
     </div>
-      <div className="div1">
+      <div className="bottom_splash" id="bottom_splash">
       <img className="img" width="400" height="300" src="/images/exa.png" />
 
         <h1 className="one">Iterate and optimize quickly without deploying code.</h1>
--- imports/ui/Splash/index.js
+++ imports/ui/Splash/index.js
@@ -11,6 +11,33 @@ export default function Splash() {
               <div className="col-md-8 col-md-offset-2">
                 <h1 className="brand-heading">SmirkSpace</h1>
                 <p className="intro-text">Connecting People Through Conversation</p>
+                <div className="row">
+                  <div className="col-md-3 col-md-offset-1">
+                    <a href="#whatwedo"> 
+                        <button type="button" className="btn btn-main">
+                          <h2> What we do <span className="glyphicon glyphicon-globe" aria-hidden="true"></span> </h2>
+                        </button>
+                    </a>
+                  </div>
+
+                  <div className="col-md-3">
+                    <a href="#usecase"> 
+                      <button type="button" className="btn btn-main">
+                          <h2> User stories <span className="glyphicon glyphicon-user" aria-hidden="true"></span> </h2>
+                      </button>
+                    </a>  
+                  </div>
+
+
+                  <div className="col-md-3">
+                    <a href="#bottom_splash"> 
+                      <button type="button" className="btn btn-main">
+                        <h2> Who we are <span className="glyphicon glyphicon-eye-open" aria-hidden="true"></span></h2> 
+                      </button>
+                    </a>
+                  </div>
+
+                </div>
               </div>
             </div>
           </div>
@@ -20,3 +47,5 @@ export default function Splash() {
     </div>
   );
 }
+
+

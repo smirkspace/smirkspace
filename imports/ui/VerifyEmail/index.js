faf155843a9fd0295e8b5d52d186ef522e0e7012
TimBaker <tbakerx@gmail.com>
2016-11-24 15:52:06 -0800

    remove comment block

--- imports/ui/VerifyEmail/index.js
+++ imports/ui/VerifyEmail/index.js
@@ -23,25 +23,3 @@ export default function VerifyEmail() {
     </div>
   );
 }
-
-/*
-<div className="modal fade">
-  <div className="modal-dialog" role="document">
-    <div className="modal-content">
-      <div className="modal-header">
-        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-        <h4 className="modal-title">Please verify your email</h4>
-      </div>
-      <div className="modal-body">
-        <p>To continue on and get chatting, please verify your email using the
-        link that was sent to you.</p>
-        <p> If you need us to resend the verification email, please click the link below.</p>
-      </div>
-      <div className="modal-footer">
-        <button type="button" className="btn btn-primary">Resend Email</button>
-      </div>
-    </div>
-  </div>
-</div>*/

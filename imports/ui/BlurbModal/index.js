9d7e044cd69822e853a2626fd956c7d331bb1467
bridgetrass <bridget.rassell@gmail.com>
2016-11-25 12:22:55 -0800

    Modal component from scratch

--- imports/ui/BlurbModal/index.js
+++ imports/ui/BlurbModal/index.js
@@ -1,36 +1,110 @@
 import React, { Component } from 'react';
-import { Transition, Modal } from 'react-overlays';
+// import { Transition, Modal } from 'react-overlays';
 import { } from 'meteor/meteor';
 
 export default class Blurb extends React.Component{
+	constructor(props) {
+      super(props)
+      	this.state = { 
+      		isModalOpen: false 
+      	}
+
+    }
+
 	render() {
 		return (
-			<div>
-				<button type="button" className="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
-				  Launch demo modal
-				</button>
-
-				<div className="modal fade" id="myModal" tabIndex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-				  <div className="modal-dialog">
-				    <div className="modal-content">
-				      <div className="modal-header">
-				        <button type="button" className="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span className="sr-only">Close</span></button>
-				        <h4 className="modal-title" id="myModalLabel">Modal title</h4>
-				      </div>
-				      <div className="modal-body">
-				        ...
-				      </div>
-				      <div className="modal-footer">
-				        <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
-				        <button type="button" className="btn btn-primary">Save changes</button>
-				      </div>
-				    </div>
-				  </div>
-				</div>
-			</div>
-	 	);
+	        <div>
+	          <button onClick={() => this.openModal()}>Open modal</button>
+	          <Modal isOpen={this.state.isModalOpen} onClose={() => this.closeModal()}>
+	            <h1> Modal title</h1>
+	            <p> hello</p>
+	            <p> <button onClick={() => this.closeModal()}>Close</button></p>
+	          </Modal>
+	        </div>
+	    );
 	}
+
+	openModal() {
+      this.setState({ isModalOpen: true })
+    }
+
+    closeModal() {
+      this.setState({ isModalOpen: false })
+    }
 }
+
+class Modal extends React.Component {
+	render() {
+      if (this.props.isOpen === false)
+        return null
+
+      let modalStyle = {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        zIndex: '9999',
+        background: '#fff'
+      }
+
+      if (this.props.width && this.props.height) {
+        modalStyle.width = this.props.width + 'px'
+        modalStyle.height = this.props.height + 'px'
+        modalStyle.marginLeft = '-' + (this.props.width/2) + 'px',
+        modalStyle.marginTop = '-' + (this.props.height/2) + 'px',
+        modalStyle.transform = null
+      }
+
+      if (this.props.style) {
+        for (let key in this.props.style) {
+          modalStyle[key] = this.props.style[key]
+        }
+      }
+
+      let backdropStyle = {
+        position: 'absolute',
+        width: '100%',
+        height: '100%',
+        top: '0px',
+        left: '0px',
+        zIndex: '9998',
+        background: '98a4b7',
+      }
+
+      if (this.props.backdropStyle) {
+        for (let key in this.props.backdropStyle) {
+          backdropStyle[key] = this.props.backdropStyle[key]
+        }
+      }
+
+      return (
+        <div className={this.props.containerClassName}>
+          <div className={this.props.className} style={modalStyle}>
+            {this.props.children}
+          </div>
+          {!this.props.noBackdrop &&
+              <div className={this.props.backdropClassName} style={backdropStyle}
+                   onClick={e => this.close(e)}/>}
+        </div>
+      )
+    }
+
+    close(e) {
+      e.preventDefault()
+
+      if (this.props.onClose) {
+        this.props.onClose()
+      }
+    }
+
+
+}
+
+
+
+
+
+
 
 
 

ae9e902fe8840f37f31e3b72534d986b3602a545
Madeline Petersen <madelineapetersen@gmail.com>
2016-11-29 19:15:44 -0800

    Addresses console errors

--- imports/ui/Guide/index.js
+++ imports/ui/Guide/index.js
@@ -10,35 +10,38 @@ export default function Guide() {
       <p>&nbsp; &nbsp;&nbsp; &nbsp;&nbsp;</p>
 
       <table className="table22">
-        <tr>
-          <td className="td11" id="whatwedo">
-            <iframe width="760" height="516" src="http://www.youtube.com/embed/pdOwkcoZfX4?rel=0&showinfo=0&autohide=1" frameBorder="0" allowFullscreen />
-          </td>
-        </tr>
-        <tr>
-          <td className="td22" id="usecase">
-            <p className="paragraph33">Check out a sample user case.<p className="paragraph44">
-            Demonstration of a real-life situation and how to go about it.</p>
-            </p>
-            <iframe className="iframe22" width="660" height="415" src="https://www.youtube.com/embed/a6zcJPBVmqg?rel=0&showinfo=0&autohide=1" frameBorder="0" allowFullscreen />
-          </td>
-        </tr>
-        <tr>
-          <td className="td44">
-            <img className="img23" alt="topics" width="500" height="450" src="/images/back5.png" />
-            <p className="paragraph77">Variety of Different Topics</p>
-            <p className="paragraph88">Choose a favourite topic or create a new one and start chatting. You will be matched
-            instantly to the first person available.
-            </p>
-          </td>
-        </tr>
-        <tr>
-          <td className="td33" id="bottom_splash">
-            <p className="paragraph55">Fun. Easy. Free</p>
-            <p className="paragraph66">Sign up today and start connecting&nbsp;<button className="go_top"><a href="#" className="scrollup">Sign up</a></button>
-            </p>
-          </td>
-        </tr>
+        <tbody>
+          <tr>
+            <td className="td11" id="whatwedo">
+              <iframe width="760" height="516" src="http://www.youtube.com/embed/pdOwkcoZfX4?rel=0&showinfo=0&autohide=1" frameBorder="0" allowFullScreen />
+            </td>
+          </tr>
+          <tr>
+            <td className="td22" id="usecase">
+              <p className="paragraph33">Check out a sample user case. </p>
+              <p className="paragraph44">
+                Demonstration of a real-life situation and how to go about it.
+              </p>
+              <iframe className="iframe22" width="660" height="415" src="https://www.youtube.com/embed/a6zcJPBVmqg?rel=0&showinfo=0&autohide=1" frameBorder="0" allowFullScreen />
+            </td>
+          </tr>
+          <tr>
+            <td className="td44">
+              <img className="img23" alt="topics" width="500" height="450" src="/images/back5.png" />
+              <p className="paragraph77">Variety of Different Topics</p>
+              <p className="paragraph88">Choose a favourite topic or create a new one and start chatting. You will be matched
+              instantly to the first person available.
+              </p>
+            </td>
+          </tr>
+          <tr>
+            <td className="td33" id="bottom_splash">
+              <p className="paragraph55">Fun. Easy. Free</p>
+              <p className="paragraph66">Sign up today and start connecting&nbsp;<button className="go_top"><a href="#" className="scrollup">Sign up</a></button>
+              </p>
+            </td>
+          </tr>
+        </tbody>
       </table>
     </div>
   );

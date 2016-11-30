4a0c079469b441dd862dfef82539591fb1b893af
TimBaker <tbakerx@gmail.com>
2016-11-29 21:36:40 -0800

    nothing

--- packages/npm-container/index.js
+++ packages/npm-container/index.js
@@ -2,7 +2,6 @@ Meteor.npmRequire = function (moduleName) {
   const module = Npm.require(moduleName);
   return module;
 };
-
 Meteor.require = function (moduleName) {
   // eslint-disable-next-line no-console
   console.warn('Meteor.require is deprecated. Please use Meteor.npmRequire instead!');

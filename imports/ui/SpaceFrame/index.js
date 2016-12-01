a83c3f3b66901eb3912e7ef04f35faffb9c9f5ee
Dahv Reinhart <dahvreinhart@hotmail.com>
2016-12-01 13:57:30 -0800

    fixed chat title, got empty box working

--- imports/ui/SpaceFrame/index.js
+++ imports/ui/SpaceFrame/index.js
@@ -70,7 +70,7 @@ export default function SpaceFrame() {
     <div className="container">
       <div className="row">
         <div className="col-md-10 col-md-offset-1 title-div">
-          <p className="topic-title">You are now talking about {topic()}    <span className="glyphicon glyphicon-thumbs-up" /></p>
+          <div className="topic-title">You are now talking about {topic()}    <span className="glyphicon glyphicon-thumbs-up" /></div>
         </div>
       </div>
       <div className="row">
@@ -87,6 +87,7 @@ export default function SpaceFrame() {
           </div>
         </div>
       </div>
+      <div className="empty-box" />
     </div>
   );
 }

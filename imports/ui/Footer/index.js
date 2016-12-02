7e431ad4563d487f66e76a3aea367fe58549740d
TimBaker <tbakerx@gmail.com>
2016-12-02 12:39:27 -0800

    Fixed the links to about us and contact us in the footer to not be black and hidden

--- imports/ui/Footer/index.js
+++ imports/ui/Footer/index.js
@@ -4,7 +4,7 @@ import './index.css';
 
 export default function Footer() {
   return (
-    <footer className="footer"><Link to="/about-us" className="link1">About us</Link> &nbsp; | &nbsp;
-    <Link to="/contact-us" className="link1">Contact us</Link></footer>
+    <footer className="footer"><Link to="/about-us" className="divButton">About us</Link> &nbsp; | &nbsp;
+    <Link to="/contact-us" className="divButton2">Contact us</Link></footer>
   );
 }
